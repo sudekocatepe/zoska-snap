2d48cc39bc2e833cc3c68867fd9ae6121b6c695b
sudekocatepe <FikriyeSudeKocatepe@s.zochova.sk>
2024-10-14 10:52:53 +0200

    error fixed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,9 +6,6 @@ export const metadata = { title : "Domov | Kutyl s.r.o." };
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      {/* Home page content */}
-    </div>
+    <Typography>Domovská stránka </Typography>
   );
 }
